a4d707feee967d38c9d95a9881aa2ebdee785329
neera12124sg <neeraj79723@gmail.com>
2025-04-05 02:50:28 +0530

    Updated index.html and script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("MIT AOE Solver Loaded!");
+
 // Toggle subject list when ASK DOUBT button is clicked
 document.getElementById('askDoubtBtn').addEventListener('click', () => {
-    const list = document.getElementById('subjectList');
-    list.style.display = list.style.display === 'none' ? 'block' : 'none';
-  });
-  
+  const list = document.getElementById('subjectList');
+  list.style.display = list.style.display === 'none' ? 'block' : 'none';
+});
